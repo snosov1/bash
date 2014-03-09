a2830b361cd2020037309fdb6b086f969cd831ba
Sergei Nosov <sergei.nosov@gmail.com>
2014-03-09 15:52:41 +0400

    cd back

--- setup-update.bash
+++ setup-update.bash
@@ -1,3 +1,5 @@
+if [[ $(id -u) -ne 0 ]] ; then echo "Please, run as sudo" ; exit 1 ; fi
+
 dir=~/.dev-setup
 
 cd $dir
@@ -5,14 +7,17 @@ cd $dir
 if [ -d "dot-emacs" ]; then
     cd dot-emacs
     git pull
+    cd ..
 fi
 
 if [ -d "bash" ]; then
     cd bin
     git pull
+    cd ..
 fi
 
 if [ -d "bin" ]; then
     cd bin
     git pull
+    cd ..
 fi
